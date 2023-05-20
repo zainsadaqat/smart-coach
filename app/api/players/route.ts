425b5e6abf4b31b0d9401b1fb00ae4c4631dc920
Rao Muhammad Akif Tufail <akifrao@gmail.com>
2023-05-20 21:25:29 +0500

    Delete route.ts

--- app/api/players/route.ts
+++
@@ -1,7 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-
-export default async function handle(req, res) {
-  const players = await prisma.player.findMany()
-  res.json(players)
-}
