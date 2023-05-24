880cea27362dd3dd09d50e053b6162670735e197
Zain Sadaqat <zain.sadaqet@gmail.com>
2023-05-24 23:16:49 +0500

    Create API routes for fetching all players

--- app/api/players/route.ts
+++ app/api/players/route.ts
@@ -1,7 +1,23 @@
 import { NextResponse } from 'next/server';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+const fetchPlayers = async () => {
+  try {
+    const players = await prisma.player.findMany({});
+    return players;
+  } catch (error: any) {
+    throw new Error(error);
+  }
+};
+
+export async function GET(request: Request) {
+  const players = await fetchPlayers();
+  return NextResponse.json({ players });
+}
 
 export async function POST(request: Request) {
   const body = await request.json();
-  console.log('Body: ', body.playerPosition);
   return NextResponse.json({ message: 'Data has been received!' });
 }
