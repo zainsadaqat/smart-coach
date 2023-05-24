774a95a609de3bc163f909f896d085159618186f
Zain Sadaqat <zain.sadaqet@gmail.com>
2023-05-24 23:17:02 +0500

    Display all players

--- app/players/page.tsx
+++ app/players/page.tsx
@@ -1,14 +1,32 @@
 'use client';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button } from '@chakra-ui/react';
 import Link from 'next/link';
 
 const Players = () => {
+  const [players, setPlayers] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('/api/players');
+      const result = await response.json();
+      setPlayers(result);
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className="my-8">
       <Link href={'/players/new'}>
         <Button colorScheme="blue">Add a Player</Button>
       </Link>
+      <h2>All Players</h2>
+      <ul>
+        {players &&
+          players.map((player) => {
+            return <li>{player?.name}</li>;
+          })}
+      </ul>
     </div>
   );
 };
