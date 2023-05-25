b06bc83bd8e062c211ab5cdc79b32076a130a5d0
Zain Sadaqat <zain.sadaqet@gmail.com>
2023-05-25 22:24:47 +0500

    Display events data on the Front End

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,14 +1,45 @@
 'use client';
 import Link from 'next/link';
-import React from 'react';
-import { Button } from '@chakra-ui/react';
+import React, { useEffect, useState } from 'react';
+import { Heading, Button } from '@chakra-ui/react';
 
 const Events = () => {
+  const [events, setEvents] = useState({
+    events: [],
+  });
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      const response = await fetch('/api/events');
+      const data = await response.json();
+      setEvents(data);
+      setLoading(false);
+    };
+    fetchEvents();
+  }, []);
+
   return (
     <div className="my-8">
       <Link href={'/events/new'}>
         <Button colorScheme="blue">Create an Event</Button>
       </Link>
+      <div className="my-4">
+        <Heading>Recent Events</Heading>
+      </div>
+      <ul className="my-4">
+        {events.events.map((event) => {
+          return (
+            <div className="flex flex-col border p-4">
+              <li>{event.event_type}</li>
+              <li>{event.date}</li>
+              <li>{event.time}</li>
+              <li>{event.location}</li>
+              <li>{event.details}</li>
+            </div>
+          );
+        })}
+      </ul>
     </div>
   );
 };
